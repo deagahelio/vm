fb6fb2a41319ef02cacc18f400cebe8768f61b37
deagahelio <16138291+deagahelio@users.noreply.github.com>
2020-04-11 22:30:15 -0300

    Clean up and comment compiler code

--- assembler/compiler.py
+++ assembler/compiler.py
@@ -12,14 +12,14 @@ class CompileError(Exception):
 
 class Compiler:
     def __init__(self, comment=False):
-        self.code = ""
-        self.funcs = []
-        self.vars = [{}]
-        self.sp_offset = 0
+        self.code = "" # Generated assembly code
+        self.funcs = [] # List of function declaration nodes
+        self.vars = [{}] # Stores variables and scopes (first scope is global)
+        self.sp_offset = 0 # Keeps track of distance from base of stack frame to store local variables
 
-        self.comment = comment
+        self.comment = comment # When set to true, will generate comments for the assembly code
 
-        self._unique_id = 0
+        self._unique_id = 0 # Used for control flow labels
 
     @staticmethod
     def is_lvalue(node):
@@ -75,12 +75,22 @@ class Compiler:
         if isinstance(node, c_ast.FuncDef):
             self.funcs.append(node.decl)
             self.sp_offset = 0
+
+            # TODO: add support for static/inline functions (and variables)
+            # $12 is used to store base pointer of function stack frame
+            # push $12 - store old value of $12 in case the caller is using it
+            # mov $15 $12 - move stack pointer to $12, stack frame starts here
+            # From now on, the stack will be used to store local variables and temporary values
             self.code += f".export #{node.decl.name}\n{node.decl.name}:\npush $12\nmov $15 $12\n"
             self.generate_expression(node.body)
+            if self.comment:
+                self.code += "; default return\n"
             if node.decl.name == "main":
-                if self.comment:
-                    self.code += "; default main return\n"
-                self.code += "mov $0 $1\nmov $12 $15\npop $12\nret\n"
+                # If no return value is specified for the main function, it should return 0
+                self.code += "mov $0 $1\n"
+            # mov $12 $15 - set stack pointer back to the start of stack frame
+            # pop $12 - pop the old $12 value that was left in the stack
+            self.code += "mov $12 $15\npop $12\nret\n"
         elif isinstance(node, c_ast.Decl):
             self.code += f".export #{node.name}\n{node.name}:\n"
             if isinstance(node.type, c_ast.TypeDecl):
@@ -90,13 +100,15 @@ class Compiler:
                     2: "word",
                     4: "dword",
                 }[self.type_size(type)]
+
                 if node.init != None:
                     if isinstance(node.init, c_ast.Constant):
                         value = node.init.value
                     else:
                         raise CompileError(f"invalid initializer type {node.init.__class__.__name__}", node)
+
                 self.code += f".{directive} {value}\n"
-                self.vars[0][node.name] = (-1, type)
+                self.vars[0][node.name] = (-1, type) # -1 since it's a global variable and not stored in the stack
             elif isinstance(node.type, c_ast.ArrayDecl):
                 type = self.type_string(node.type)
                 directive = {
@@ -104,11 +116,14 @@ class Compiler:
                     2: "word",
                     4: "dword",
                 }[self.type_size(type)]
+
                 if isinstance(node.type.dim, c_ast.Constant):
                     size = int(node.type.dim.value)
                 else:
                     raise CompileError(f"invalid array dim type {node.type.dim.__class__.__name__}", node)
+
                 if node.init != None:
+                    # Cut initializer list if it exceeds array size
                     elems = node.init.exprs[:size]
                     for elem in elems:
                         if isinstance(elem, c_ast.Constant):
@@ -116,8 +131,10 @@ class Compiler:
                         else:
                             raise CompileError(f"invalid initializer type {elem.__class__.__name__}", node)
                     size -= len(elems)
+
                 if size > 0:
                     self.code += f".{directive} 0 {size}\n"
+
                 self.vars[0][node.name] = (-1, type)
             else:
                 raise CompileError(f"unknown decl type {node.type.__class__.__name__}", node)
@@ -125,31 +142,44 @@ class Compiler:
             raise CompileError(f"unknown node {node.__class__.__name__}", node)
     
     def generate_expression(self, node, register=1):
+        # This function is recursive
+        # The generated code will store the value of the expression in $register
+        # and the address of the value (if there is one) in $register+1
         if isinstance(node, c_ast.Compound):
+            # Push a new local variable scope (stack frame doesn't change)
             self.vars.append({})
+
             if node.block_items != None:
                 for item in node.block_items:
                     self.generate_expression(item, register=register)
+
             self.vars.pop()
         elif isinstance(node, c_ast.Return):
             if self.comment:
                 self.code += "; return\n"
+
+            # Return value is stored in $1
             self.generate_expression(node.expr)
             self.code += "mov $12 $15\npop $12\nret\n"
         elif isinstance(node, c_ast.Decl):
             if isinstance(node.type, c_ast.TypeDecl):
                 if self.comment:
                     self.code += f"; variable {node.name}\n"
+
                 if node.init == None:
+                    # Moving $0 uses less space than using an immediate value
                     self.code += f"mov $0 ${register}\n"
                 else:
                     self.generate_expression(node.init, register=register)
+
                 self.code += f"push ${register}\n"
                 self.sp_offset -= 4
                 self.vars[-1][node.name] = (self.sp_offset, self.type_string(node.type))
             elif isinstance(node.type, c_ast.ArrayDecl):
                 if self.comment:
                     self.code += f"; variable (array) {node.name}\n"
+
+                # Assumes node.type.dim is constant TODO
                 size = int(node.type.dim.value)
                 self.code += "push $0\n" * size
                 self.sp_offset -= 4 * size
@@ -159,9 +189,11 @@ class Compiler:
         elif isinstance(node, c_ast.Assignment):
             if self.comment:
                 self.code += "; assignment\n"
+
             if node.op == "=":
                 if not self.is_lvalue(node.lvalue):
                     raise CompileError(f"expr not lvalue {node.lvalue}", node)
+
                 self.generate_expression(node.lvalue, register=register)
                 self.code += f"push ${register+1}\n"
                 type = self.generate_expression(node.rvalue, register=register)
@@ -172,11 +204,14 @@ class Compiler:
         elif isinstance(node, c_ast.ArrayRef):
             if self.comment:
                 self.code += "; array ref\n"
+
             if not self.is_lvalue(node.name):
                 raise CompileError(f"expr not lvalue {node.name}", node)
+
             type = self.generate_expression(node.name, register=register)
             if type[-1] != "*":
                 raise CompileError(f"can't use array ref on non-pointer value", node)
+
             self.code += f"push ${register+1}\n"
             self.generate_expression(node.subscript, register=register)
             self.code += f"mul {self.type_size(type[:-1])} ${register}\nmov $13 ${register}\n"
@@ -185,6 +220,9 @@ class Compiler:
         elif isinstance(node, c_ast.For):
             if self.comment:
                 self.code += "; for loop\n"
+
+            # Generating a compound expression already makes a new scope so this one
+            # only contains variables defined in the initial loop statement
             self.vars.append({})
             id = self.unique_id
             for decl in node.init:
@@ -199,11 +237,14 @@ class Compiler:
         elif isinstance(node, c_ast.BinaryOp):
             if self.comment:
                 self.code += "; binary op\n; right expr\n"
+
             self.generate_expression(node.right, register=register)
             self.code += f"push ${register}\n"
+
             if self.comment:
                 self.code += "; left expr\n"
             type = self.generate_expression(node.left, register=register)
+
             self.code += f"pop ${register+1}\n"
             self.code += {
                 "+": f"add ${register+1} ${register}\n",
@@ -215,9 +256,11 @@ class Compiler:
         elif isinstance(node, c_ast.UnaryOp):
             if self.comment:
                 self.code += "; unary op\n"
+
             if node.op == "p++":
                 if not self.is_lvalue(node.expr):
                     raise CompileError(f"expr not lvalue {node.expr}", node)
+
                 type = self.generate_expression(node.expr, register=register)
                 self.code += f"add 1 ${register}\nstd ${register} ${register+1}\n"
                 return type
@@ -232,16 +275,17 @@ class Compiler:
         elif isinstance(node, c_ast.Cast):
             if self.comment:
                 self.code += "; cast\n"
+
             self.generate_expression(node.expr, register=register)
-            type = node.to_type.type.type.type.names[0]
-            if isinstance(node.to_type.type, c_ast.PtrDecl):
-                type += "*"
+            type = self.type_string(node.to_type.type)
             return type
         elif isinstance(node, c_ast.Constant):
             if self.comment:
                 self.code += "; constant\n"
+
             if node.type == "int":
                 if node.value == "0":
+                    # Moving $0 uses less space than using an immediate value
                     self.code += f"mov $0 ${register}\n"
                 else:
                     self.code += f"mov {node.value} ${register}\n"
@@ -251,15 +295,17 @@ class Compiler:
         elif isinstance(node, c_ast.ID):
             if self.comment:
                 self.code += f"; load variable {node.name}\n"
+
             var = self.get_var(node.name)
             if var != None:
                 (offset, type) = var
-                if offset == -1:
+                if offset == -1: # Variable is global
                     self.code += f"mov #{node.name} ${register+1}\nldd ${register+1} ${register}\n"
                 else:
                     self.code += f"mov $12 ${register+1}\nsub {-offset} ${register+1}\nldd ${register+1} ${register}\n"
             else:
                 raise CompileError(f"unknown variable {node.name}", node)
+
             return type
         elif isinstance(node, c_ast.EmptyStatement):
             pass
